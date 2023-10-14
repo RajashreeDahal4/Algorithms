0c51a406a668410ee4132536e545b984c36ff387
rajashree_dahal-at-278255066410 <RajashreeDahal4>
2023-10-14 08:24:48 -0500

    maximum number of k_sum pairs

---
+++ binary_search_problems/leetcode/max_number_of_K_sum_pairs.py
@@ -0,0 +1,8 @@
+'''
+Date: October 14, 2023
+Name: Rajashree Dahal
+Problem: Max Numbe of K-Sum Pairs
+You are given an integer array nums and an integer k.
+In one operation, you can pick two numbers from the array whose 
+sum equals k and remove them from the array.Return the maximum number of operations you can perform on the array.
+'''
