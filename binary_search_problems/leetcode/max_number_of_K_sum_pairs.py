4ad30ec48b944e8b9542a32cbf7fffb15179096e
rajashree_dahal-at-278255066410 <RajashreeDahal4>
2023-10-14 22:36:33 -0500

    find the combination of parenthesis for a given value of n

--- binary_search_problems/leetcode/max_number_of_K_sum_pairs.py
+++ binary_search_problems/leetcode/max_number_of_K_sum_pairs.py
@@ -6,3 +6,25 @@ You are given an integer array nums and an integer k.
 In one operation, you can pick two numbers from the array whose 
 sum equals k and remove them from the array.Return the maximum number of operations you can perform on the array.
 '''
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        count_dict={}
+        max_count=0
+        #forming a dictionary that keeps track of count of each element in the list
+        for i in nums:
+            if i not in count_dict.keys():
+                count_dict[i]=1
+            else:
+                count_dict[i]=count_dict[i]+1
+        for i in nums:
+            if i in count_dict.keys() and count_dict[i]>0:
+                remaining=k-i
+                count_dict[i]=count_dict[i]-1
+                if remaining in count_dict.keys() and count_dict[remaining]>0:
+                    max_count=max_count+1
+                    count_dict[remaining]=count_dict[remaining]-1
+                else:
+                    count_dict[i]=count_dict[i]+1
+        return max_count
+
---
+++ divide_and_conquer/dominating_element.py
@@ -0,0 +1,7 @@
+'''
+Date: October 5, 2023
+Name: Rajashree Dahal
+Problem: Use divide and conquer approach to find the dominating element of an array
+Definition: A dominating element is the one which appears more than n/2 times in the array
+'''
+
