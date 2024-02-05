65856b4f76d550d36e0445772d78ce5c6a267ce0
rajashree_dahal-at-278255066410 <RajashreeDahal4>
2024-02-05 09:15:06 -0600

    Solution Script prepared for removing stars from a string

---
+++ 2390_removing_stars_from_a_string.py
@@ -0,0 +1,26 @@
+"""
+Date: February 5, 2024
+Name: Rajashree Dahal
+Problem:You are given a string s, which contains stars *.
+
+In one operation, you can:
+
+Choose a star in s.
+Remove the closest non-star character to its left, as well as remove the star itself.
+Return the string after all stars have been removed.
+
+Note:
+The input will be generated such that the operation is always possible.
+It can be shown that the resulting string will always be unique.
+"""
+class Solution:
+    def removeStars(self, s: str) -> str:
+        result=[]
+        for each_char in s:
+            if each_char=="*" and len(result)!=0:
+                result.pop()
+            elif each_char=="*":
+                continue
+            else:
+                result.append(each_char)
+        return "".join(result)
