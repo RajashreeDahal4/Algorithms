ac689e4ff8a267117eb08d212395b4d34442ae45
rajashree_dahal-at-278255066410 <RajashreeDahal4>
2024-02-14 16:44:02 -0600

    Solution script prepared for generate parentheses problem

---
+++ 22_Generate_Parentheses.py
@@ -0,0 +1,30 @@
+"""
+Date: February 14, 2024
+Name: Rajashree Dahal
+Problem: Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+"""
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def backtrack(s='', open_count=0, close_count=0):
+            if len(s) == 2 * n:
+                result.append(s)
+                return
+            if open_count < n:
+                backtrack(s+'(', open_count+1, close_count)
+            if close_count < open_count:
+                backtrack(s+')', open_count, close_count+1)
+
+        result = []
+        backtrack()
+        return result
