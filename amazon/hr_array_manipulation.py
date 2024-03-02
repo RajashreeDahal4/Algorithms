9bc84bd005810205a4e015fa4f5501f1fe50b393
rajashree_dahal-at-278255066410 <RajashreeDahal4>
2024-03-01 20:54:44 -0600

    Optimized code writted for array Manipulation Problem

--- amazon/hr_array_manipulation.py
+++ amazon/hr_array_manipulation.py
@@ -7,7 +7,6 @@ Starting with a 1-indexed array of zeros and a list of operations, for each oper
 def arrayManipulation(n, queries):
     # Write your code here
     result=[0]*n
-    print(result)
     for each_query in queries:
         start=each_query[0]
         end=each_query[1]
@@ -15,3 +14,18 @@ def arrayManipulation(n, queries):
         result=[(i+value) if (enum<=(end-1) and enum>=(start-1)) else i for enum,i in enumerate(result)]
 
     return max(result)
+
+
+def optimized_arrayManipulation(n, queries):
+    # Write your code here
+    result=[0]*n
+    for start,end,value in queries:
+        result[start-1]=result[start-1]+value 
+        if end<len(result):
+            result[end]-=value 
+    max_value = 0
+    running_total = 0
+    for value in result:
+        running_total += value
+        max_value = max(max_value, running_total)
+    return max_value
