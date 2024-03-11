58073b35bf308ab2009590c30b869f9c83355d4b
rajashree_dahal-at-278255066410 <RajashreeDahal4>
2024-03-11 15:13:57 -0500

    Solutino script prepared for appending characters to string to make subsequence problem

---
+++ Amazon_premium/2486_Append_characters_to_string_to_Make_subsequence.py
@@ -0,0 +1,19 @@
+"""
+Date: March 11, 2024
+Name: Rajashree Dahal
+Problem:
+You are given two strings s and t consisting of only lowercase English letters.
+Return the minimum number of characters that need to be appended to the end of s so that t becomes a subsequence of s.
+A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+"""
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        #First, we try to make a list out of a string
+        t=list(t)
+        counter=0
+        for i in s:
+            if len(t)==0:
+                return 0
+            if i ==t[0]:
+                t.pop(0)
+        return len(t)
